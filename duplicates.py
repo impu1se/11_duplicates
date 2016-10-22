b1b7cfe32512fc1dc9d35627af92304a45317900
impu1se <impu1se@bk.ru>
2016-10-22 23:12:52 +0300

    fin

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,18 @@
+import os
+import sys
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def are_files_duplicates(file_path):
+    res = []
+    for d, dirs, file in os.walk(file_path):
+        for f in file:
+            res.append(os.path.join(d, f))
+    return res
 
+def find_file_duplicates(file_list):
+    for file in file_list:
+        for 
 
 if __name__ == '__main__':
-    pass
+    print(are_files_duplicates(sys.argv[1]))
+
