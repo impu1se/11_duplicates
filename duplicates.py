3a9c422a91cf5d28fab838416976e890962b08b4
impu1se <impu1se@bk.ru>
2016-10-31 23:53:38 +0300

    Half

--- duplicates.py
+++ duplicates.py
@@ -1,20 +1,29 @@
 import os
 import sys
+import binascii
 
 
-def are_files_duplicates(file_path):
+def get_files_list(file_path):
     file_in_dirs = []
-    for d, dirs, file in os.walk(file_path):
-        for f in file:
-            if not f.startswith('.') and os.path.isfile(os.path.join(d, f)):
-                file_in_dirs.append(os.path.join(d, f))
+    for d, dirs, files in os.walk(file_path):
+        for file in files:
+            if not file.startswith('.'):
+                file_in_dirs.append(os.path.join(d, file))
     return file_in_dirs
 
+
 def find_file_duplicates(file_list):
-    pass
+    file_with_sum = {}
+    for file in file_list:
+        with open(file, 'rb') as f:
+            if binascii.crc32(f.peek()) in file_with_sum:
+                print(file,'\t' + file_with_sum[binascii.crc32(f.peek())])
+            else:
+                file_with_sum[binascii.crc32(f.peek())] = file
+    return file_with_sum
 
 
 if __name__ == '__main__':
-    file_list = are_files_duplicates(sys.argv[1]))
+    file_list = get_files_list(sys.argv[1])
     find_file_duplicates(file_list)
 
